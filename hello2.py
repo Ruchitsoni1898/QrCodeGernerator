9d154351af46a1f6bd470119f2ec8f1cdfa093cf
Ruchit Soni <ruchitsoni1898@gmail.com>
2024-08-26 12:39:30 +0200

    if else and elif programme

---
+++ hello2.py
@@ -0,0 +1,9 @@
+password = "hello21"
+pass_input = input("Enter password:")
+
+while password != pass_input:
+    pass_input = input("Enter password:")
+
+else:
+    print("password is unlocked!!!!")
+
---
+++ hello3.py
@@ -0,0 +1,9 @@
+#if "a" in "Great Work":
+    #pass
+
+for number in range(1,11):
+    if number % 2 == 0:
+        break
+    else:
+        print(number)
+
